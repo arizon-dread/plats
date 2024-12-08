7f6d5bb7c29120b60419c6ececd0067682a71462
Erik Svensson <arizon.es@gmail.com>
2024-12-09 00:49:08 +0100

    refactor + add interface

---
+++ internal/database/cache.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/arizon-dread/plats/internal/config"
+	"github.com/gomodule/redigo/redis"
+)
+
+type Cache struct {
+}
+
+func (c *Cache) Store(key string, value any) error {
+	conn, err := conn()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	conn.Do("SET", key, value)
+	return nil
+}
+
+func (c *Cache) Get(key string) *string {
+	conn, err := conn()
+	if err != nil {
+		return nil
+	}
+	val, err := redis.String(conn.Do("GET", key))
+	if err != nil {
+		return nil
+	}
+	return &val
+}
+
+func conn() (redis.Conn, error) {
+	conf := config.Config{}
+	conf.Load()
+
+	conn, err := redis.Dial(conf.Cache.Proto, conf.Cache.Url)
+	if err != nil {
+		return nil, fmt.Errorf("error getting redis connection %v", err)
+	}
+	return conn, nil
+}
--- internal/database/db.go
+++ internal/database/db.go
@@ -1,44 +1,6 @@
 package database
 
-import (
-	"fmt"
-
-	"github.com/arizon-dread/plats/internal/config"
-	"github.com/gomodule/redigo/redis"
-)
-
-type Db struct {
-}
-
-func (c *Db) Store(key string, value any) error {
-	conn, err := conn()
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	conn.Do("SET", key, value)
-	return nil
-}
-
-func (c *Db) Get(key string) *string {
-	conn, err := conn()
-	if err != nil {
-		return nil
-	}
-	val, err := redis.String(conn.Do("GET", key))
-	if err != nil {
-		return nil
-	}
-	return &val
-}
-
-func conn() (redis.Conn, error) {
-	conf := config.Config{}
-	conf.Load()
-
-	conn, err := redis.Dial(conf.Cache.Proto, conf.Cache.Url)
-	if err != nil {
-		return nil, fmt.Errorf("error getting redis connection %v", err)
-	}
-	return conn, nil
+type Db interface {
+	Store(string, any) error
+	Get(string) *string
 }
